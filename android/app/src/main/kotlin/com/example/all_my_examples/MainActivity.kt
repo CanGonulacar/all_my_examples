5833957b9baacf29202ec6f17ad88a2efd98c965
Can Gönülaçar <89852107+CanGonulacar@users.noreply.github.com>
2024-10-22 15:28:23 +0300

    Start

---
+++ android/app/src/main/kotlin/com/example/all_my_examples/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.all_my_examples
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:all_my_examples/home_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const _MyApp());
+}
+
+class _MyApp extends StatelessWidget {
+  const _MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
